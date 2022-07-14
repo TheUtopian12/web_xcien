73adb6767aaefa21b08f28feed3c1ba70763bf55
Edwin Castruita <edwi.castruita117@gmail.com>
2022-07-13 19:06:03 -0500

    first commit

---
+++ src/pages/Service/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Services = () => {
+  return (
+    <div className="container">
+    
+        <h1 className="text-center" style={{paddingTop: "30%"}}>
+          Servicios
+        </h1>
+        
+    </div>
+  )
+}
+export default Services;
